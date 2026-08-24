770ad7920ea44ab7e690c40d602939a7464b4dc4
cameron Johnson <CameronJohnson12244@gmail.com>
2026-08-24 00:17:26 -0500

    improved fire script

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -1,34 +1,45 @@
 using UnityEngine;
 
+
 public class Fire : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float baseSize = 1f;
-    public float growthRate = 0.1f;
+    public float growthRate = 0.5f;
     public float maxSize = 5f;
 
     private float currentSize;
+    private float Targetgrowth;//setting a target for the growth for each interation
     private int TrashBurned = 0;
+    private bool Kindling = false;
 
     void Start()
     {
         
-        currentSize = transform.localScale.x;
-        
+        currentSize = baseSize;
+        transform.localScale = new Vector3(currentSize, currentSize, currentSize);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (Kindling)
+        {
+            GrowFire(growthRate * Time.deltaTime);
+            if (currentSize >= Targetgrowth)
+            {
+            Kindling = false; //reset kindling when growth hits target
+            }
+        }
+        if (currentSize >= maxSize)
+        {
+            currentSize = maxSize;
+            Kindling = false; //stop growth past max size
+        }
     }
     private void GrowFire(float amount)
     {
         currentSize += amount;
-        if (currentSize > maxSize)//limit the fire size
-        {
-            currentSize = maxSize;
-        }
         transform.localScale = new Vector3(currentSize, currentSize, currentSize);
          
     }
@@ -40,9 +51,11 @@ public class Fire : MonoBehaviour
             TrashBurned++;
             if(TrashBurned >= 3)//grow the fire after 3 trash objects are destroyed
             {
-                GrowFire(0.5f);
+                Targetgrowth = Mathf.Clamp(currentSize + growthRate, baseSize, maxSize);
+                Kindling = true;
                 TrashBurned = 0; //reset counter
             }
+            
             
             
         }
