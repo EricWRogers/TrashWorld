565545e841c32d7b3f33f3ea769153f8bce3e5b9
Joshua Deaton <joshwdeaton@gmail.com>
2026-08-25 00:18:56 -0500

    Trash spawner and pick up trash
    Trash spawns randomly every 5 seconds and can be grabbed by the player.

--- Assets/Scripts/GarbageCollector.cs
+++ Assets/Scripts/GarbageCollector.cs
@@ -8,7 +8,7 @@ public class GarbageCollector : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        garbageObjects.AddRange(GameObject.FindGameObjectsWithTag("Garbage"));
+        garbageObjects.AddRange(GameObject.FindGameObjectsWithTag("Trash"));
     }
 
     // Update is called once per frame
